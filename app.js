8da06637dfb95c14f1600a2a5337bb6795db1556
manuisazaa <123569719+manuisazaa@users.noreply.github.com>
2023-02-06 12:03:09 -0500

    JavaScript

---
+++ app.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardsAdj = [
+    {
+      name: "1",
+      img: "imagenes/1.jpg"
+    },
+    {
+      name: "2",
+      img: "imagenes/2.jpg"
+    },
+    {
+      name: "3",
+      img: "imagenes/3.jpg"
+    },
+    {
+      name: "4",
+      img: "imagenes/4.jpg"
+    },
+    {
+      name: "5",
+      img: "imagenes/5.jpg"
+    },
+    {
+      name: "6",
+      img: "imagenes/6.jpg"
+    },
+    {
+      name: "1",
+      img: "imagenes/1.jpg"
+    },
+    {
+      name: "2",
+      img: "imagenes/2.jpg"
+    },
+    {
+      name: "3",
+      img: "imagenes/3.jpg"
+    },
+    {
+      name: "4",
+      img: "imagenes/4.jpg"
+    },
+    {
+      name: "5",
+      img: "imagenes/5.jpg"
+    },
+    {
+      name: "6",
+      img: "imagenes/6.jpg"
+    }
+  ];
+});
