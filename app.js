049a10f0debba762b4185c668eafd2f8c54641f7
manuisazaa <123569719+manuisazaa@users.noreply.github.com>
2023-03-08 09:14:20 -0500

    cambios 8/03/2023

--- app.js
+++ app.js
@@ -49,10 +49,31 @@ document.addEventListener("DOMContentLoaded", () => {
       img: "imagenes/6.jpg"
     }
   ];
-});
 
-const cuadricula = document.querySelector(".cuadricula");
-const resultado = document.querySelector("#resultado");
-var cartasEsogidas =[];
-var cartasEscogidasId =[];
-var cartasGanadas =[];
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
+
+  function crearTablero() {
+    for (let i = 0; i < cardsAdj.length; i++) {
+      var carta = document.createElement("img");
+      carta.setAttribute("src", "imagenes/reverso.png");
+      carta.setAttribute("data-id", i);
+      carta.addEventListener("click", voltearCarta);
+      cuadricula.appendChild(carta);
+    }
+  }
+
+  function voltearCarta() {
+    var cardId = this.getAttribute("data-id");
+    cartasEscogidas.push(cardsAdj[cardId].name);
+    cartasEscogidasId.push(cardId);
+    this.setAttribute("src", cardsAdj[cardId].img);
+    if (cartasEscogidas.length === 2) {
+      setTimeout(verificarPareja, 1000);
+    }
+  }
+  crearTablero();
+});
